6578d2fe85f059925d6902f06d6d98b8f206ea91
Rohith Ravi <Rohithravi@srmtech.com>
2022-12-23 14:46:30 +0530

    Initial Message

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//var name = "Rohith";
+
+app.get('/', (req, res) => res.send('Hello All!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
